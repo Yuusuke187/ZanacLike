8077bcc761d14e872aa3a5fdebb075f3b30a3926
Paxton <paxton.perry@protonmail.com>
2024-10-17 21:56:46 -0700

    Renamed _spawn_timer to spawn_timer and also in the last commit I added logic for the spawn timer and the logic for spawning new enemies

--- enemy.gd
+++ enemy.gd
@@ -11,3 +11,17 @@ func _process(delta: float) -> void:
 	pass
 
 var enemy_scene = preload("res://First_Enemy.tscn")
+var timer = 0.0
+var spawn_rate = 2.0
+
+func spawn_timer(delta):
+	timer += delta
+	if timer > spawn_rate
+		timer = 0
+		# spawn_enemy()
+
+func spawn_enemy():
+	var enemy = enemy_scene.instance()
+	# I still need to figure out how Vector2 works
+	enemy.position = Vector2(rand_range(0, screen_width), -50)
+	get_parent().add_child(enemy)
